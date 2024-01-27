473d3d5de00731baa769fb611c35329af191eacc
GeorgeLianos <cs.glianos@gmail.com>
2024-01-27 13:11:45 +0000

    Create session

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import GlobalStyles from "../../assets/css/GlobalStyles.module.css"
+import useMultiplayer from "../../multiplayer/useMultiplayer";
 import LandingPageStyles from "./LandingPage.module.css"
 import classnames from 'classnames'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+    const navigate = useNavigate();
+    const { createRoom } = useMultiplayer(); 
+    const [showLoader, setShowLoader] = useState<boolean>(false)
+
+    const onCreateRoom = () => {
+        setShowLoader(true);
+        createRoom().then(data => {
+            if(data.roomId) {
+                navigate("/join-session/" + data.roomId)
+            }
+        }).finally(() => setShowLoader(false))
+    }
+
     return (
             <div 
             className={classnames(
@@ -11,8 +26,9 @@ const LandingPage = () => {
                 )}
             >
                 <div
+                    onClick={onCreateRoom}
                     className={classnames(LandingPageStyles.button, LandingPageStyles.createButton, GlobalStyles.flex1, GlobalStyles.centerVertical)}
-                >Create Session</div>
+                >{showLoader ? "Loading..." : "Create Session"}</div>
                 <Link
                     to="/join-session"
                     className={classnames(LandingPageStyles.button, LandingPageStyles.joinButton, GlobalStyles.flex1, GlobalStyles.centerVertical)}
